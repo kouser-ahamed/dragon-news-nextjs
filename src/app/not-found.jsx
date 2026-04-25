f2ff9aaccbe4fe0b044f6d641991b0c5cc90addc
kouser-ahamed <ahamed15-5643@diu.edu.bd>
2026-04-26 01:01:16 +0600

    Add NotFound component for handling 404 errors

---
+++ src/app/not-found.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className='h-[80vh] flex justify-center items-center flex-col'>
+            <h2 className='font-bold text-5xl text-purple-500'>This page is not found</h2>
+        </div>
+    );
+};
+
+export default NotFound;
