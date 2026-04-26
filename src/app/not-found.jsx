ac772e046f7209358c0be3e4549b3df4a0daa958
kouser-ahamed <ahamed15-5643@diu.edu.bd>
2026-04-26 11:16:48 +0600

    Enhance NotFound component with navigation button to return home

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
 const NotFound = () => {
-    return (
-        <div className='h-[80vh] flex justify-center items-center flex-col'>
-            <h2 className='font-bold text-5xl text-purple-500'>This page is not found</h2>
-        </div>
-    );
+  return (
+    <div className="h-[80vh] flex justify-center items-center flex-col gap-4">
+      <h2 className="font-bold text-5xl text-purple-500">
+        This page is not found
+      </h2>
+      <Link href={"/"}>
+        <button className="btn bg-purple-500 text-white hover:bg-blue-500 rounded-xl">
+          Back to Home
+        </button>
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
