768326080be0d95dfa4b34a84219352bbe9dca20
kouser-ahamed <ahamed15-5643@diu.edu.bd>
2026-04-26 12:12:10 +0600

    Refactor layout components to use Montserrat and Poppins fonts for improved typography

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -1,14 +1,14 @@
-import Navbar from '@/components/shared/Navbar';
-import React from 'react';
+import Navbar from "@/components/shared/Navbar";
+import React from "react";
+import { montserrat } from "../layout";
 
-const AuthLayout = ({children}) => {
-    return (
-        <>
-        <Navbar />
-        {children}
-            
-        </>
-    );
+const AuthLayout = ({ children }) => {
+  return (
+    <div className={`${montserrat.className}`}>
+      <Navbar />
+      {children}
+    </div>
+  );
 };
 
 export default AuthLayout;
